f90ea3776125c3a77785608619e6cc0630a9a5ed
rbklein <rbk@cwi.nl>
2024-02-06 15:32:15 +0100

    differentiation matrix docstring added

--- programs/differentiation.py
+++ programs/differentiation.py
@@ -9,12 +9,23 @@ Namespace for differentiation operators
 """
 
 def differentiation_matrix_1D(points):
+    """
+    Compute differentiation matrix of 1D Lagrange polynomials associated points i.e. D_ij = dh_j/dx(x_i) with h_j(x) the Lagrange polynomial 
+    with value one in points[i] and zeros in points[j] with j not i
+    """
+    if len(points) < 1:
+        raise ValueError('error: at least one point required')
+    if len(points) == 1:
+        return 0
+
     n = len(points)
     D = np.zeros((n,n))
     for i in range(n):
         D = D.at[:,i].set(polynomials.lagrange_polynomial_derivative(points, points, i))
     return D
 
+#def differentiation_matrix_2D(points):
+#    n = len(points)
 
 if __name__ == "__main__":
     import quadrature
